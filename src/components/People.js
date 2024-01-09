0ba25ccccc1d59a405446abad2f79549135da836
VinzentErtl <vinzent.ertl@student.htldornbirn.at>
2024-01-09 09:38:17 +0100

    finished with the cards

--- src/components/People.js
+++ src/components/People.js
@@ -2,28 +2,47 @@ import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
 export default function People() {
-const [people, setPeople] = useState([]);
-const [color, setColor] = useState("green");
+  const [people, setPeople] = useState([]);
+  const [filterdPeople, setFilterdPeople] = useState([]);
 
   useEffect(() => {
     fetch("https://65814cf53dfdd1b11c42e639.mockapi.io/person").then((res) =>
       res.json().then((data) => {
-       setPeople(data);
+        setPeople(data);
+        setFilterdPeople(data);
       })
     );
   }, []);
 
-  useEffect(()=>{
-    alert("change")
-  }, [color])
+  const filterPeople = (filter) =>{
+    let filterd = people.filter(person => person.name.includes(filter));
+    setFilterdPeople(filterd);
+  }
 
   return (
-    <div>
+    <div className="p-8">
       <h1>People</h1>
-      <button onClick={()=>{setColor("orange")}}>Change Color</button>
-      <div className="grid grid-cols-2 lg:grid-cols-4">
-        {people.map(person => {
-            return <Card name={person.name} title={person.jobtitle} imageUri={person.avatar} />
+      <div className="border">
+        <input
+          className="border p-4"
+          type="text"
+          placeholder="Search"
+          onChange={(el) => {
+            console.log(el.target.value);
+            filterPeople(el.target.value);
+          }}
+        />
+      </div>
+
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        {filterdPeople.map((person) => {
+          return (
+            <Card
+              name={person.name}
+              title={person.jobtitle}
+              imageUri={person.avatar}
+            />
+          );
         })}
       </div>
     </div>
