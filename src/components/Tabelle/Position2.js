a27135f799c6fc7584a7546b32a1a457e99f85b5
VinzentErtl <vinzent.ertl@student.htldornbirn.at>
2024-03-12 08:11:11 +0100

    finished

--- src/components/Tabelle/Position2.js
+++ src/components/Tabelle/Position2.js
@@ -4,7 +4,7 @@ export default function Position2({ index, team }) {
 
   console.log("team", team);
   return (
-    <div className="w-[26rem] border-solid border-[0.1rem] border-black p-2 my-2 flex flex-row content-between items-center hover:bg-white bg-slate-400 grid grid-cols-[40px_40px_auto_40px]">
+    <div className="w-[26rem] border-solid border-[0.1rem] border-black p-2 my-2 flex flex-row content-between items-center hover:bg-white bg-[#D3D3D3] grid grid-cols-[40px_40px_auto_40px]">
       <div className="w-1/10 pl-2 ">{index + 1}</div>
       <div className="w-5">
         <img src={team.teamIconUrl} />
